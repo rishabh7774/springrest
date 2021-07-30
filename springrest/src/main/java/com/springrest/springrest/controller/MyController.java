2b9d9b0c40c74a9527b77d2fd84bd2e28f58c358
Rishabh Gupta <rishabhg@Rishabhs-MacBook-Pro.local>
2021-07-31 03:36:27 +0530

    Added_Swagger

--- springrest/src/main/java/com/springrest/springrest/controller/MyController.java
+++ springrest/src/main/java/com/springrest/springrest/controller/MyController.java
@@ -3,18 +3,13 @@ package com.springrest.springrest.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.springrest.springrest.services.*;
 
 import com.springrest.springrest.entities.*;
 
 @RestController
+@RequestMapping("/api")
 public class MyController {
 	
 	@Autowired
--- springrest/src/main/java/com/springrest/springrest/services/courseServiceImpl.java
+++ springrest/src/main/java/com/springrest/springrest/services/courseServiceImpl.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Service;
 
 import com.springrest.springrest.entities.Course;
 
-import jdk.internal.org.jline.utils.Log;
 
 @Service
 public class courseServiceImpl implements courseService {
