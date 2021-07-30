d2b3587b412a8dfc41a96f73985d5d2db3acfbde
Rishabh Gupta <rishabhg@Rishabhs-MacBook-Pro.local>
2021-07-31 03:34:26 +0530

    Test

--- springrest/src/main/java/com/springrest/springrest/SpringrestApplication.java
+++ springrest/src/main/java/com/springrest/springrest/SpringrestApplication.java
@@ -2,12 +2,15 @@ package com.springrest.springrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SpringrestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringrestApplication.class, args);
+
 	}
 
 }
